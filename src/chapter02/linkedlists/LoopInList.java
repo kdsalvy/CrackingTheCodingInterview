b52b0bccc9b21ad28bd727da5521f92a6601f77e
Saurabh Kedia <kedia.saurabh@gmail.com>
2018-04-06 11:06:08 +0530

    Changing the logic a bit

--- src/chapter02/linkedlists/LoopInList.java
+++ src/chapter02/linkedlists/LoopInList.java
@@ -10,81 +10,116 @@ import chapter02.linkedlists.LinkedList.Node;
  */
 public class LoopInList {
 
-	public static void main(String[] args) {
-		LinkedList list = new LinkedList();
-		list.add(1);
-		list.add(2);
-		list.add(3);
-		list.add(4);
-		list.add(5);
-		list.add(6);
-		list.add(7);
-		list.add(8);
-		list.add(9);
 
-		LoopInList lil = new LoopInList();
+    public static void main(String[] args) {
+        LinkedListLoop lil = new LinkedListLoop();
+        LinkedList list = lil.new LinkedList();
+       for(int i = 1; i <= 6; i++){
+           list.add(i);
+       }
 
-		list = lil.linkLastAndMiddleNode(list);
-		Node loopStartingNode = lil.getStartingNodeOfLoop(list);
-		loopStartingNode.n = null;
-		System.out.println(loopStartingNode);
+       list = lil.linkLastAndMiddleNode(list);
+       Node loopStartingNode = lil.getStartingNodeOfLoop(list);
+       loopStartingNode.next = null;
+       System.out.println(loopStartingNode);
+    }
 
-	}
+    public LinkedList linkLastAndMiddleNode(LinkedList list) {
+        if (list.head == null)
+            return list;
+        Node mNode = getMiddleNode(list);
+        Node lNode = getLastNode(list);
 
-	public LinkedList linkLastAndMiddleNode(LinkedList list) {
-		if (list.head == null)
-			return list;
-		Node mNode = getMiddleNode(list);
-		Node lNode = getLastNode(list);
+        lNode.next = mNode;
+        return list;
+    }
 
-		lNode.n = mNode;
-		return list;
-	}
+    private Node getLastNode(LinkedList list) {
+        Node p = list.head;
+        while (p.next != null)
+            p = p.next;
+        return p;
+    }
 
-	private Node getLastNode(LinkedList list) {
-		Node p = list.head;
-		while (p.n != null)
-			p = p.n;
-		return p;
-	}
+    private Node getMiddleNode(LinkedList list) {
+        Node p1 = list.head.next;
+        Node p2 = list.head;
 
-	private Node getMiddleNode(LinkedList list) {
-		Node p1 = list.head.n;
-		Node p2 = list.head;
+        while (p1 != null) {
+            p1 = p1.next;
+            if (p1 != null) {
+                p2 = p2.next;
+                p1 = p1.next;
+            }
+        }
+        return p2;
+    }
 
-		while (p1 != null) {
-			p1 = p1.n;
-			if (p1 != null) {
-				p2 = p2.n;
-				p1 = p1.n;
-			}
-		}
-		return p2;
-	}
+    public Node getStartingNodeOfLoop(LinkedList list) {
+        if (list.head == null || list.head.next == null)
+            return list.head;
+        Node n1 = list.head;
+        Node n2 = list.head;
+        Node n3 = list.head;
 
-	public Node getStartingNodeOfLoop(LinkedList list) {
-		if (list.head == null)
-			return list.head;
-		Node n1 = list.head.n;
-		Node n2 = list.head;
-		Node n3 = list.head;
+        n1 = n1.next;
+        n2 = n2.next.next;
 
-		// n1 = n2 will confirm that the loop exists in the list
-		while (n1 != null && n2 != null && n1 != n2) {
-			n1 = n1.n;
-			if (n1 != null) {
-				n2 = n2.n;
-				n1 = n1.n;
-			}
-		}
+        // n1 = n2 will confirm that the loop exists in the list
+        while (n1 != null && n2 != null && n1 != n2) {
+            n1 = n1.next;
+            n2 = n2.next.next;
+        }
 
-		// when n3 will reach n2, n1 will reach the start of the loop
-		while (n3 != n2) {
-			n3 = n3.n;
-			n1 = n1.n;
-		}
+        // n2 and n3 will reach the meeting point together as if m is the distance from head till
+        // start of the loop, k is the distance from start of the loop till the meeting point and
+        // n is the length of the loop then m+k will be a multiple of n if we solve the equation
+        // distance travelled by fast pointer = 2 * distance travelled by slow pointer
+        if(n1 == n2) {
+            while (n3.next != n1.next) {
+                n3 = n3.next;
+                n1 = n1.next;
+            }
+        }
 
-		return n3;
-	}
+        if(n3.next == n1.next)
+            return n3.next;
+        else
+            return null;
+    }
+
+    class LinkedList{
+        Node head;
+
+        public void add(int data){
+            if(head == null)
+                head = new Node(data);
+            else {
+                Node temp = head;
+                while(temp.next != null)
+                    temp = temp.next;
+                temp.next = new Node(data);
+            }
+        }
+
+        @Override
+        public String toString(){
+            return head.toString();
+        }
+    }
+
+    class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+        }
+
+        @Override
+        public String toString(){
+            return "[ " + data + " ] -> " + next;
+        }
+    }
 
 }
